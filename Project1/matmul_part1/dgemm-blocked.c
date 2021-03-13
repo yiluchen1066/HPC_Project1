822808162968c26db808fc361fad11af71d69ec6
Yilu Chen <chenyilu2016@outlook.com>
2021-03-13 16:33:23 +0100

    loop and avx optimized

--- Project1/matmul_part1/dgemm-blocked.c
+++ Project1/matmul_part1/dgemm-blocked.c
@@ -22,49 +22,63 @@ const char* dgemm_desc = "Blocked, three-loop dgemm.";
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
  * On exit, A and B maintain their input values. */    
-void square_dgemm (int n, double* A, double* B, double* C)
+void square_dgemm(int n, double* A, double* B, double* C)
 {
   // TODO: Implement the blocking optimization
-  __m256d ymm0, ymm1, ymm2; 
+  __m256d ymm0, ymm1; 
+  __m256i zero = _mm256_setzero_si256();
+  __m256d zerod = _mm256_setzero_pd();
+  __m256i mask1 = _mm256_setr_epi32(1,1,0,0,0,0,0,0);
+  __m256i mask2 = _mm256_setr_epi32(1,1,1,1,0,0,0,0);
+  __m256i mask3 = _mm256_setr_epi32(1,1,1,1,1,1,0,0);
+  __m256i masks[4] = {zero, mask1, mask2, mask3};
+  __m128i idx = _mm_setr_epi32(0, n, 2*n, 3*n);
   int s = 8; 
   int b = n/s+1; 
-  __attribute__((aligned(32))) double as[s]; 
-  __attribute__((aligned(32))) double buff[4]; 
-  for (int i = 0; i < b; i++)
+  int res = (n - (n/s)*s)%4;
+  __m256i mask = masks[res];
+  __m256d maskd =  _mm256_castsi256_pd(mask);
+  #pragma omp parallel for
+  for (int i = 0; i < n; i = i+s)
   {
-    for (int j = 0; j < b; j++)
+    for (int j = 0; j < n; j = j+s)
     {
-      for (int k = 0; k < b; k++)
+      for (int k = 0; k < n; k = k+s)
       {
-        for (int si = 0; si < s && (si+s*i) < n; si++)
+        int limi = i+s;
+        int limj = j+s; 
+        int limk = k+s; 
+        limi = limi < n ? limi:n; 
+        limj = limj < n ? limj:n; 
+        limk = limk < n ? limk:n; 
+        for (int si = i; si < limi; si++)
         {
-          for (int sj = 0; sj < s && (sj+s*j) < n; sj++)
+          #pragma GCC unroll 4
+          for (int sj = j; sj < limj; sj++)
           {
-            double cij = C[s*i+si+n*(s*j+sj)]; 
-            int sk, ssk; 
-            for (sk = 0; sk < s && (sk+s*k) < n ; sk++)
-            { 
-              as[sk] = A[i*s+si+n*(s*k+sk)]; 
-            }
-            for (ssk = 0; ssk+4 < sk; ssk = ssk+4)
+            double cij = C[si+sj*n]; 
+            int ssk; 
+            #pragma GCC unroll 2
+            for (ssk = 0; ssk+4 < limk-k; ssk = ssk+4)
             {
-              ymm0 = _mm256_load_pd(&as[ssk]); 
-              ymm1 = _mm256_loadu_pd(&B[k*s+ssk+n*(sj+s*j)]); 
+              ymm0 = _mm256_i32gather_pd(&A[si+n*(k+ssk)], idx, 1);
+              ymm1 = _mm256_loadu_pd(&B[ssk+k+n*sj]); 
               //ymm1 = _mm256_mul_pd(ymm0, ymm1); 
-              _mm256_store_pd(buff, _mm256_mul_pd(ymm0, ymm1)); 
+              cij += hsum_double_avx(_mm256_mul_pd(ymm0, ymm1));
             }
-            cij += buff[0]+buff[1]+buff[2]+buff[3]; 
-            for (; ssk < sk ; ssk++)
-            {
-              cij += as[ssk]*B[k*s+ssk+n*(sj+s*j)]; 
+            if (k+ssk < limk) {
+                ymm0 = _mm256_mask_i32gather_pd(zerod, &A[si+n*(k+ssk)], idx, maskd, 1);
+                ymm1 = _mm256_maskload_pd(&B[ssk+k+n*sj], mask);
+                cij += hsum_double_avx(_mm256_mul_pd(ymm0, ymm1));
             }
-            C[s*i+si+n*(sj+s*j)]=cij; 
+            C[si+n*sj]=cij;
           }
         } 
       }
     }
   }
 }
+
 
 
 
