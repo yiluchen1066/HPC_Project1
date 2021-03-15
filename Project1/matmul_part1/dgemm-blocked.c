56af7408aba93a90cfa82a9eb88acd490e42b7bb
Yilu Chen <chenyilu2016@outlook.com>
2021-03-15 17:33:58 +0100

    optimize avx

--- Project1/matmul_part1/dgemm-blocked.c
+++ Project1/matmul_part1/dgemm-blocked.c
@@ -29,7 +29,7 @@ inline double hsum_double_avx(__m256d v) {
 
 
 
-void square_dgemm(int n, double* A, double* B, double* C)
+void square_dgemm_unroll(int n, double* A, double* B, double* C)
 {
   // TODO: Implement the blocking optimization
   __m256i zero = _mm256_setzero_si256();
@@ -142,14 +142,23 @@ void square_dgemm(int n, double* A, double* B, double* C)
   }
 }
 
+#define _MM_HINT_T0 1
+#define _MM_HINT_T1 2
+#define _MM_HINT_T2 3
+#define _MM_HINT_NTA 0
+#define _MM_HINT_ENTA 4
+#define _MM_HINT_ET0 5
+#define _MM_HINT_ET1 6
+#define _MM_HINT_ET2 7
+
 
 /* This routine performs a dgemm operation
  *  C = C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
  * On exit, A and B maintain their input values. */  
 
-/*
-void square_dgemm_avx(int n, double* A, double* B, double* C)
+
+void square_dgemm(int n, double* A, double* B, double* C)
 {
   // TODO: Implement the blocking optimization
   __m256d ymm0, ymm1;
@@ -160,7 +169,7 @@ void square_dgemm_avx(int n, double* A, double* B, double* C)
   __m256i mask3 = _mm256_setr_epi32(1,1,1,1,1,1,0,0);
   __m256i masks[4] = {zero, mask1, mask2, mask3};
   __m128i idx = _mm_setr_epi32(0, n, 2*n, 3*n);
-  int s = 4; 
+  const int s = 8; 
   int res = (n - (n/s)*s)%4;
   __m256i mask = (masks[res] != zero);
   __m256d maskd =  _mm256_castsi256_pd(mask);
@@ -177,42 +186,75 @@ void square_dgemm_avx(int n, double* A, double* B, double* C)
       {
         int limi = i+s; 
         limi = limi < n ? limi:n; 
+        if (limk < n && limi < n && limj < n) {
+          _mm_prefetch(&C[i+8+n*j], _MM_HINT_ENTA);
+          _mm_prefetch(&C[i+8+n*(j+1)], _MM_HINT_ENTA);
+          _mm_prefetch(&C[i+8+n*(j+2)], _MM_HINT_ENTA);
+          _mm_prefetch(&C[i+8+n*(j+3)], _MM_HINT_ENTA);
+          _mm_prefetch(&C[i+8+n*(j+4)], _MM_HINT_ENTA);
+          _mm_prefetch(&C[i+8+n*(j+5)], _MM_HINT_ENTA);
+          _mm_prefetch(&C[i+8+n*(j+6)], _MM_HINT_ENTA);
+          _mm_prefetch(&C[i+8+n*(j+7)], _MM_HINT_ENTA);
+          /*
+          _mm_prefetch(&A[i+8+n*k], _MM_HINT_T0);
+          _mm_prefetch(&A[i+8+n*(k+1)], _MM_HINT_T0);
+          _mm_prefetch(&A[i+8+n*(k+2)], _MM_HINT_T0);
+          _mm_prefetch(&A[i+8+n*(k+3)], _MM_HINT_T0);
+          _mm_prefetch(&A[i+8+n*(k+4)], _MM_HINT_T0);
+          _mm_prefetch(&A[i+8+n*(k+5)], _MM_HINT_T0);
+          _mm_prefetch(&A[i+8+n*(k+6)], _MM_HINT_T0);
+          _mm_prefetch(&A[i+8+n*(k+7)], _MM_HINT_T0);
+          */
+          for (int sj = j; sj < j+s; sj += 4)
+            for (int si = i; si < i+s; si += 4)
+              for (int sk = k; sk < k+s; sk += 4) {
+                __m256d b00, b01, b02, b03, b10, b11, b12, b13, b20, b21, b22, b23, b30, b31, b32, b33;
+                __m256d c0, c1, c2, c3, a0, a1, a2, a3;
+                b00 = _mm256_set1_pd(B[sk+n*(sj+0)]);
+                b10 = _mm256_set1_pd(B[sk+1+n*(sj+0)]);
+                b20 = _mm256_set1_pd(B[sk+2+n*(sj+0)]);
+                b30 = _mm256_set1_pd(B[sk+3+n*(sj+0)]);
+                b01 = _mm256_set1_pd(B[sk+n*(sj+1)]);
+                b11 = _mm256_set1_pd(B[sk+1+n*(sj+1)]);
+                b21 = _mm256_set1_pd(B[sk+2+n*(sj+1)]);
+                b31 = _mm256_set1_pd(B[sk+3+n*(sj+1)]);
+                b02 = _mm256_set1_pd(B[sk+n*(sj+2)]);
+                b12 = _mm256_set1_pd(B[sk+1+n*(sj+2)]);
+                b22 = _mm256_set1_pd(B[sk+2+n*(sj+2)]);
+                b32 = _mm256_set1_pd(B[sk+3+n*(sj+2)]);
+                b03 = _mm256_set1_pd(B[sk+n*(sj+3)]);
+                b13 = _mm256_set1_pd(B[sk+1+n*(sj+3)]);
+                b23 = _mm256_set1_pd(B[sk+2+n*(sj+3)]);
+                b33 = _mm256_set1_pd(B[sk+3+n*(sj+3)]);
+                a0 = _mm256_loadu_pd(&A[si+n*sk]);
+                a1 = _mm256_loadu_pd(&A[si+n*(sk+1)]);
+                a2 = _mm256_loadu_pd(&A[si+n*(sk+2)]);
+                a3 = _mm256_loadu_pd(&A[si+n*(sk+3)]);
+                c0 = _mm256_loadu_pd(&C[si+sj*n]);
+                c1 = _mm256_loadu_pd(&C[si+(sj+1)*n]);
+                c2 = _mm256_loadu_pd(&C[si+(sj+2)*n]);
+                c3 = _mm256_loadu_pd(&C[si+(sj+3)*n]);
+                c0 = _mm256_add_pd(_mm256_fmadd_pd(a0, b00, _mm256_fmadd_pd(a1, b10, c0)), _mm256_fmadd_pd(a2, b20, _mm256_mul_pd(a3, b30)));
+                c1 = _mm256_add_pd(_mm256_fmadd_pd(a0, b01, _mm256_fmadd_pd(a1, b11, c1)), _mm256_fmadd_pd(a2, b21, _mm256_mul_pd(a3, b31)));
+                c2 = _mm256_add_pd(_mm256_fmadd_pd(a0, b02, _mm256_fmadd_pd(a1, b12, c2)), _mm256_fmadd_pd(a2, b22, _mm256_mul_pd(a3, b32)));
+                c3 = _mm256_add_pd(_mm256_fmadd_pd(a0, b03, _mm256_fmadd_pd(a1, b13, c3)), _mm256_fmadd_pd(a2, b23, _mm256_mul_pd(a3, b33)));
+                _mm256_storeu_pd(&C[si+sj*n], c0);
+                _mm256_storeu_pd(&C[si+(sj+1)*n], c1);
+                _mm256_storeu_pd(&C[si+(sj+2)*n], c2);
+                _mm256_storeu_pd(&C[si+(sj+3)*n], c3);
+              }
+        } else
         for (int sj = j; sj < limj; sj++)
-        
         {
-          if (limk < n && limi < n) {
-            __m256d a00, a01, a02, a03, a10, a11, a12, a13, a20, a21, a22, a23, a30, a31, a32, a33;
-            __m256d c0, c1, c2, c3, b0, b1, b2, b3;
-            a00 = _mm256_set1_pd(A[i+n*(k+0)]);
-            a10 = _mm256_set1_pd(A[i+1+n*(k+0)]);
-            a20 = _mm256_set1_pd(A[i+2+n*(k+0)]);
-            a30 = _mm256_set1_pd(A[i+3+n*(k+0)]);
-            b0 = _mm256_loadu_pd(&B[k+n*j]);
-            c0 = _mm256_loadu_pd(&C[i+j*n]);
-            c0 = _mm256_fmadd_pd(a00, b0,  );
-
-            a01 = _mm256_set1_pd(A[i+n*(k+1)]);
-            a11 = _mm256_set1_pd(A[i+1+n*(k+1)]);
-            a21 = _mm256_set1_pd(A[i+2+n*(k+1)]);
-            a31 = _mm256_set1_pd(A[i+3+n*(k+1)]);
-            a02 = _mm256_set1_pd(A[i+n*(k+2)]);
-            a12 = _mm256_set1_pd(A[i+1+n*(k+2)]);
-            a22 = _mm256_set1_pd(A[i+2+n*(k+2)]);
-            a32 = _mm256_set1_pd(A[i+3+n*(k+2)]);
-            a03 = _mm256_set1_pd(A[i+n*(k+3)]);
-            a13 = _mm256_set1_pd(A[i+1+n*(k+3)]);
-            a23 = _mm256_set1_pd(A[i+2+n*(k+3)]);
-            a33 = _mm256_set1_pd(A[i+3+n*(k+3)]);
-          }
           for (int si = i; si < limi; si++)
           {
             double cij = C[si+sj*n]; 
             int ssk; 
             for (ssk = 0; ssk+4 < limk-k+1; ssk = ssk+4)
             {
-              ymm0 = _mm256_i32gather_pd(&A[si+n*(k+ssk)], idx, 8);
-              ymm1 = _mm256_loadu_pd(&B[ssk+k+n*sj]); 
-              cij += hsum_double_avx(_mm256_mul_pd(ymm0, ymm1));
+                ymm0 = _mm256_i32gather_pd(&A[si+n*(k+ssk)], idx, 8);
+                ymm1 = _mm256_loadu_pd(&B[ssk+k+n*sj]); 
+                cij += hsum_double_avx(_mm256_mul_pd(ymm0, ymm1));
             }
             if (k+ssk < limk) {
                 ymm0 = _mm256_mask_i32gather_pd(zerod, &A[si+n*(k+ssk)], idx, maskd, 8);
@@ -227,7 +269,6 @@ void square_dgemm_avx(int n, double* A, double* B, double* C)
   }
 }
 
-*/
 // https://stackoverflow.com/questions/16737298/what-is-the-fastest-way-to-transpose-a-matrix-in-c
 #define _MM256_TRANSPOSE4_PD(row0, row1, row2, row3) { \
   __m256d tmp3, tmp2, tmp1, tmp0, v; \
